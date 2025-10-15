2144425c28b7f77666d75d6b81f1304af7427a45
dcarbf02 <dcarbf02@estudiantes.unileon.es>
2025-10-15 14:46:29 +0200

    Transitions layer and files created

---
+++ Frontend/Programa1/Style/Transition/DefaultTransition.cs
@@ -0,0 +1,19 @@
+using Avalonia;
+using Avalonia.Media;
+using Programa1.layer;
+using Programa1.Layer.Bridge;
+
+namespace Programa1.Style.Background;
+
+public class DefaultBackground : IBackground {
+    
+    public void Render(RenderContext ctx) {
+        ctx.Display(
+            draw: dc => {
+               dc.FillRectangle(Brushes.White, new Rect(ctx.Bounds.Size)); 
+            }
+        );
+
+    }
+    
+}
