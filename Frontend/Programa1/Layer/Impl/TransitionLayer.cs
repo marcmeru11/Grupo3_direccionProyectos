7cdbb060c94c01d18a1b754b0c7ee88d095b227f
acordc02 <acordc02@estudiantes.unileon.es>
2025-10-22 14:24:29 +0200

    Default Transition test 2

--- Frontend/Programa1/Layer/Impl/TransitionLayer.cs
+++ Frontend/Programa1/Layer/Impl/TransitionLayer.cs
@@ -4,9 +4,8 @@ using Programa1.Style;
 namespace Programa1.Layer.Impl;
     
 public class TransitionLayer : IRenderLayer {
-        public void Render(RenderContext ctx, RenderStyle style)
-        {
-            // Llama al render de transición definido en RenderStyle
-            style.RenderTransition(ctx);
-        }
+    
+    public void Render(RenderContext ctx, RenderStyle style) {
+        style.RenderTransition(ctx);
     }
+}
