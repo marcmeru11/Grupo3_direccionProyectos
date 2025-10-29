ec0b3a786e112fa0725b0b732d99c07f95e7d772
dcarbf02 <dcarbf02@estudiantes.unileon.es>
2025-10-29 13:55:36 +0100

    Slide1Transition name changed and RenderStyle configured to show Slide2 as default

--- Frontend/Programa1/Style/RenderStyle.cs
+++ Frontend/Programa1/Style/RenderStyle.cs
@@ -21,7 +21,7 @@ public class RenderStyle {
     public static RenderStyle Default() {
         return new RenderStyle(
             new DefaultBackground(),
-            new DefaultTransition()
+            new SlideTransition1()
         );
     }
 
