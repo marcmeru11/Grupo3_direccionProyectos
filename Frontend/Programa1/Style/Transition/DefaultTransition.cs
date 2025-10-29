dead9de9b6e8a43f1a6d536d5d81325070a155e8
acordc02 <acordc02@estudiantes.unileon.es>
2025-10-29 13:52:47 +0100

    Slide2Transition

--- Frontend/Programa1/Style/Transition/DefaultTransition.cs
+++ Frontend/Programa1/Style/Transition/DefaultTransition.cs
@@ -2,40 +2,47 @@
 using Avalonia.Media;
 using Programa1.layer;
 using Programa1.Layer.Bridge;
+using System;
 
-namespace Programa1.Style.Transition;
-
-public class DefaultTransition : ITransition
+namespace Programa1.Style.Transition
 {
-    private double _opacity = 1;
-    private bool _fadeIn = false;
-
-    public void Render(RenderContext ctx)
+    public class DefaultTransition : ITransition
     {
-        const double speed = 0.02;
-        if (_fadeIn)
+        private double _opacity = 0;       
+        private int _phase = 0;          
+        private readonly double _speed;
+        private readonly SolidColorBrush _brush;
+
+        public DefaultTransition(double speed = 0.02)
         {
-            _opacity += speed;
+            _speed = speed;
+            _brush = new SolidColorBrush(Colors.Black, _opacity);
+        }
+
+        public void Render(RenderContext ctx)
+        {
+            if (_phase == 2)
+                return;
+
+            _opacity += (_phase == 0 ? _speed : -_speed);
+
             if (_opacity >= 1)
             {
                 _opacity = 1;
-                _fadeIn = false;
+                _phase = 1; 
             }
-        }
-        else
-        {
-            _opacity -= speed;
-            if (_opacity <= 0)
+            else if (_opacity <= 0 && _phase == 1)
             {
                 _opacity = 0;
-                _fadeIn = true;
+                _phase = 2; 
             }
-        }
 
-        ctx.Display(dc =>
-        {
-            var brush = new SolidColorBrush(Colors.Black, _opacity);
-            dc.FillRectangle(brush, new Rect(ctx.Bounds.Size));
-        });
+            _brush.Opacity = _opacity;
+
+            ctx.Display(dc =>
+            {
+                dc.FillRectangle(_brush, new Rect(ctx.Bounds.Size));
+            });
+        }
     }
 }
---
+++ Frontend/Programa1/Style/Transition/Slide2Transition.cs
@@ -0,0 +1,60 @@
+﻿using Avalonia;
+using Avalonia.Media;
+using Programa1.layer;
+using Programa1.Layer.Bridge;
+using System;
+
+namespace Programa1.Style.Transition
+{
+    public class Slide2Transition : ITransition
+    {
+        private double _progress = 0;       
+        private int _phase = 0;            
+        private readonly double _speed;
+
+        public Slide2Transition(double speed = 0.02)
+        {
+            _speed = speed;
+        }
+
+        public void Render(RenderContext ctx)
+        {
+            if (_phase == 2)
+                return;
+
+            _progress += _speed;
+            if (_progress > 1) _progress = 1;
+
+            double offsetX = 0;
+
+            switch (_phase)
+            {
+                case 0: 
+                    offsetX = ctx.Bounds.Width * (1 - _progress);
+                    break;
+                case 1: 
+                    offsetX = -ctx.Bounds.Width * _progress;
+                    break;
+            }
+
+            ctx.Display(dc =>
+            {
+                var brush = new SolidColorBrush(Colors.Black);
+                dc.FillRectangle(brush, new Rect(offsetX, 0, ctx.Bounds.Width, ctx.Bounds.Height));
+            });
+
+            if (_progress >= 1)
+            {
+                if (_phase == 0)
+                {
+                    _phase = 1;
+                    _progress = 0;
+                }
+                else if (_phase == 1)
+                {
+                    _phase = 2;
+                }
+            }
+        }
+    }
+}
