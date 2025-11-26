4c39c6a831f00cff0a06f7a652d742e5e82dd06f
M1k3l05 <malvaf12@estudiantes.unileon.es>
2025-11-26 14:17:19 +0100

    changed main.py to yolo.py in order to adapt it to the API

--- Backend/YOLO/yolo.py
+++ Backend/YOLO/yolo.py
@@ -1,5 +1,7 @@
 import cv2
 from ultralytics import YOLO
+import base64
+import numpy
 
 """
 This module defines the `YOLOMdoel` class — a lightweight interface for loading a YOLO model
@@ -62,6 +64,17 @@ class YOLOMdoel:
             name : str, optional
                 Subdirectory name for this specific run (default is "predict").
     """
+    def evaluate_image(image_path):
+        predict(self, image_path)
+
+    def img_to_base64(image_array):
+        success, buffer = cv2.imencode(".png", img_array)
+        if not success:
+            raise ValueError("No se pudo convertir la imagen a PNG")
+
+        img_base64 = base64.b64encode(buffer).decode("utf-8")
+        return img_base64
+    
     def predict(self, image_path, conf_threshold=0.5, save=False, project="runs/detect", name="predict"):
         if self.model is None:
             print("El modelo no está cargado.")
@@ -76,9 +89,19 @@ class YOLOMdoel:
             
             self.last_prediction = results
             annotated_frame = results[0].plot()
+            annotated_base64 = img_to_base64(annotated_frame)
+            #TODO pasar imagen (annotated_frame) a base64
+            if int(results[0].boxes.cls[0]) == 0:
+                status = "NORMAL"
+            else:
+                status = "ANOMALY"
+
+            result = {
+                "reconstructed_base": annotated_base64, #TODO annotated_base64 poner,
+                "status": status
+            }
             
-            
-            return annotated_frame, int(results[0].boxes.cls[0])
+            return result
         except Exception as e:
             print(f"Error during prediction: {e}")
             return None
@@ -90,53 +113,6 @@ class YOLOMdoel:
         The window will remain open until a key is pressed.
         Requires that `predict()` or `predict_multiple()` has been called first.
     """
-    def show_last_prediction(self):
-
-        annotated_frame = self.last_prediction[0].plot() 
-        cv2.imshow("YOLO Result", annotated_frame)
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
-    
-    
-    """
-        Performs predictions on multiple images.
-
-        Parameters:
-        
-            image_paths : list[str]
-                A list of image file paths to process.
-            conf_threshold : float, optional
-                Minimum confidence threshold (default is 0.5).
-            save : bool, optional
-                Whether to save annotated images to disk (default is False).
-            project : str, optional
-                Directory where results will be stored (default is "runs/detect").
-            name : str, optional
-                Subdirectory name for this specific run (default is "predict").
-    """
-    def predict_multiple(self, image_paths, conf_threshold=0.5, save=False, project="runs/detect", name="predict"):
-        if self.model is None:
-            print("El modelo no está cargado.")
-            return None
-
-        predictions = []
-        for image_path in image_paths:
-            try:
-                results = self.model(image_path,
-                                     conf=conf_threshold, 
-                                     save=save, 
-                                     project=project, 
-                                     name=name)
-                
-                
-                predictions.append(results)
-            except Exception as e:
-                print(f"Error during prediction for {image_path}: {e}")
-                predictions.append(None)
-        
-        self.last_prediction = results
-        return predictions
-    
     
 """
     Main execution block.
@@ -146,7 +122,3 @@ class YOLOMdoel:
 """
 if __name__ == "__main__":
     yolo = YOLOMdoel("best.pt")
-    annotated_frame, result = yolo.predict("Data/cropped_images/img_2025-01-31_10.46.48_11786819_cam_H2674069.png", conf_threshold=0.25, save=False)
-    print("the class bellongs to the class: ", result)
-    yolo.show_last_prediction()
-
